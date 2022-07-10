915a3e87b81893e9373945456e3572922af9e18f
SoftwareArchitect9817 <derekjones9817@gmail.com>
2022-07-10 02:39:22 +0200

    windowsassembly

--- app/Models/WIndowsAssembly.php
+++ app/Models/WIndowsAssembly.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class WIndowsAssembly extends Model
+class WindowsAssembly extends Model
 {
     protected $table = "windowsassembly";
 }
